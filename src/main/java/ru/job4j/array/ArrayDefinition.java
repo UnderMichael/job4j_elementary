df0211c9276f949b7e992b6e735f90d70a484df2
underMichael <m.podddorogin@gmail.com>
2025-04-04 09:40:59 +0300

    Размер массива [#505221]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -5,5 +5,8 @@ public class ArrayDefinition {
 		short[] ages = new short[10];
 		String[] surnames = new String[100500];
 		float[] prices = new float[40];
+		System.out.println("length of ages is " + ages.length);
+		System.out.println("length of surnames is " + surnames.length);
+		System.out.println("length of prices is " + prices.length);
 	}
 }
