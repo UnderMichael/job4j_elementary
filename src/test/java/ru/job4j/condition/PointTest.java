22945f25760698d07da4e451f907916bed7b6abf
underMichael <m.podddorogin@gmail.com>
2025-04-09 19:45:42 +0300

    4. Расстояние между точками в трехмерном пространстве.

--- src/test/java/ru/job4j/condition/PointTest.java
+++ src/test/java/ru/job4j/condition/PointTest.java
@@ -15,10 +15,26 @@ class PointTest {
 	}
 
 	@Test
+	void whenPoints04And4Minus5AndMinus4532Then75Dot43() {
+		int x1 = 0, y1 = 2, x2 = 4, y2 = -5, z1 = -43, z2 = 32;
+		double expected = 75.43;
+		double output = new Point(x1, y1, z1).distance3d(new Point(x2, y2, z2));
+		assertThat(output).isEqualTo(expected, withPrecision(0.01));
+	}
+
+	@Test
 	void whenPoints36And3Minus4Then7Dot61() {
 		int x1 = 3, y1 = 3, x2 = 6, y2 = -4;
 		double expected = 7.61;
 		double output = new Point(x1, y1).distance(new Point(x2, y2));
+		assertThat(output).isEqualTo(expected, withPrecision(0.01));
+	}
+
+	@Test
+	void whenPoints36And3Minus4AndMinus34Then10Dot34() {
+		int x1 = 3, y1 = 3, x2 = 6, y2 = -4, z1 = -3, z2 = 4;
+		double expected = 10.34;
+		double output = new Point(x1, y1, z1).distance3d(new Point(x2, y2, z2));
 		assertThat(output).isEqualTo(expected, withPrecision(0.01));
 	}
 
@@ -31,10 +47,26 @@ class PointTest {
 	}
 
 	@Test
+	void whenPoints06And0Minus4And34Then7Dot28() {
+		int x1 = 0, y1 = 0, x2 = 6, y2 = -4, z1 = 3, z2 = 4;
+		double expected = 7.28;
+		double output = new Point(x1, y1, z1).distance3d(new Point(x2, y2, z2));
+		assertThat(output).isEqualTo(expected, withPrecision(0.01));
+	}
+
+	@Test
 	void whenPointsMinus2Minus6AndMinus6Minus4Then4Dot47() {
 		int x1 = -2, y1 = -6, x2 = -6, y2 = -4;
 		double expected = 4.47;
 		double output = new Point(x1, y1).distance(new Point(x2, y2));
+		assertThat(output).isEqualTo(expected, withPrecision(0.01));
+	}
+
+	@Test
+	void whenPointsMinus2Minus6AndMinus6Minus4And73Then6() {
+		int x1 = -2, y1 = -6, x2 = -6, y2 = -4, z1 = 7, z2 = 3;
+		double expected = 6;
+		double output = new Point(x1, y1, z1).distance3d(new Point(x2, y2, z2));
 		assertThat(output).isEqualTo(expected, withPrecision(0.01));
 	}
 }
