c25d2cff372495bd7a2875ec79393114edabba70
underMichael <m.podddorogin@gmail.com>
2025-04-09 19:00:57 +0300

    2.0. Рефакторинг - Расстояние между точками.

--- src/test/java/ru/job4j/condition/PointTest.java
+++ src/test/java/ru/job4j/condition/PointTest.java
@@ -10,7 +10,7 @@ class PointTest {
 	void whenPoints04And4Minus5Then8Dot06() {
 		int x1 = 0, y1 = 2, x2 = 4, y2 = -5;
 		double expected = 8.06;
-		double output = Point.distance(x1, y1, x2, y2);
+		double output = new Point(x1, y1).distance(new Point(x2, y2));
 		assertThat(output).isEqualTo(expected, withPrecision(0.01));
 	}
 
@@ -18,7 +18,7 @@ class PointTest {
 	void whenPoints36And3Minus4Then7Dot61() {
 		int x1 = 3, y1 = 3, x2 = 6, y2 = -4;
 		double expected = 7.61;
-		double output = Point.distance(x1, y1, x2, y2);
+		double output = new Point(x1, y1).distance(new Point(x2, y2));
 		assertThat(output).isEqualTo(expected, withPrecision(0.01));
 	}
 
@@ -26,7 +26,7 @@ class PointTest {
 	void whenPoints06And0Minus4Then7Dot21() {
 		int x1 = 0, y1 = 0, x2 = 6, y2 = -4;
 		double expected = 7.21;
-		double output = Point.distance(x1, y1, x2, y2);
+		double output = new Point(x1, y1).distance(new Point(x2, y2));
 		assertThat(output).isEqualTo(expected, withPrecision(0.01));
 	}
 
@@ -34,7 +34,7 @@ class PointTest {
 	void whenPointsMinus2Minus6AndMinus6Minus4Then4Dot47() {
 		int x1 = -2, y1 = -6, x2 = -6, y2 = -4;
 		double expected = 4.47;
-		double output = Point.distance(x1, y1, x2, y2);
+		double output = new Point(x1, y1).distance(new Point(x2, y2));
 		assertThat(output).isEqualTo(expected, withPrecision(0.01));
 	}
 }
